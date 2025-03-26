68f13efe34691c522eccfd9ebaca968bed4fea63
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-03-26 19:11:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const fs = require('fs');
 // set up rate limiter: maximum of 100 requests per 15 minutes
 const limiter = RateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 100 requests per windowMs
+  max: 10000, // max 100 requests per windowMs
 });
 const https = require('https');
 
