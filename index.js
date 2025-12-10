c01b48b295ec05bbe54e3b9d7feb4544c94c954e
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-12-10 15:46:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function get(url, dest) {
                 else fs.unlink(dest, () => reject(err.message)); // Delete temp file
               });
               response.pipe(file);
-            } else if (response.statusCode === 302 or response.statusCode === 301) {
+            } else if (response.statusCode === 302 || response.statusCode === 301) {
               //Recursively follow redirects, only a 200 will resolve.
               get(response.headers.location, dest).then(() => resolve());
             } else {
